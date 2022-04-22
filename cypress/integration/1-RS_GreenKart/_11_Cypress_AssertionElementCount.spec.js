2be101c94614645c7a6e6e7f79625a12d986bf2d
“rajatt95” <rajatvermaa95@gmail.com>
2022-04-22 18:08:27 +0530

    Spec file names updated (.js to .spec.js)

---
+++ cypress/integration/1-RS_GreenKart/_11_Cypress_AssertionElementCount.spec.js
@@ -0,0 +1,18 @@
+
+//Testing Framework - Mocha
+
+describe('Test Suite - Rahul Shetty Green Kart - _03_Cypress_Type',function(){
+
+    it('Test case for _03_Cypress_Type',function(){
+
+        //Test One -> Steps
+        //Selenium WebDriver -> driver.get("https://rahulshettyacademy.com/seleniumPractise/");
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/')
+        
+        //Selenium WebDriver -> driver.findElement(By.cssSelector(".search-keyword")).sendKeys("ca");
+        cy.get('.search-keyword').should('have.length',1);
+
+
+    })//if
+
+})//describe
