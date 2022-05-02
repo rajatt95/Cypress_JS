65f9ef59ac6896589fe34c350feec4b9d033a825
“rajatt95” <rajatvermaa95@gmail.com>
2022-05-03 02:10:28 +0530

    Details for RS Cypress_BDD project added

---
+++ cypress/integration/Section_14_RS_BDD_SeparateProject/ProjectInfo.js
@@ -0,0 +1,17 @@
+/**  
+* @author Rajat Verma
+* https://www.linkedin.com/in/rajat-v-3b0685128/
+* https://github.com/rajatt95
+* https://rajatt95.github.io/
+* 
+* Course: Cypress -Modern Automation Testing from Scratch + Frameworks(https://www.udemy.com/course/cypress-tutorial/)
+* Tutor: Rahul Shetty (https://www.udemy.com/user/rahul445/)
+*/
+
+/***************************************************/
+
+/**
+ * Project name: Cypress_BDD 
+ * https://github.com/rajatt95/Cypress_BDD
+ * About: Automation Testing | Web | Cypress | Javascript | VS Code | Mocha | Cucumber | Cucumber-HTML-Reports
+*/
